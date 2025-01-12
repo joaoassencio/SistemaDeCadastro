8d1b86790b6e4791fee6a5110e0d7b4554877f5d
João Assêncio <joao.assencio123@gmail.com>
2025-01-12 10:32:10 -0500

    first commit

---
+++ src/test/java/dev/cadastros/SistemaDeCadastro/SistemaDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cadastros.SistemaDeCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
