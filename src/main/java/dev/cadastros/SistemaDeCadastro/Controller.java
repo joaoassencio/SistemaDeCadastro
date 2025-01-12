f0bbae4e062a8e26c2f47f3de6d2626677df2d8b
João Assêncio <joao.assencio123@gmail.com>
2025-01-12 12:12:55 -0500

    Controller file created

---
+++ src/main/java/dev/cadastros/SistemaDeCadastro/Controller.java
@@ -0,0 +1,14 @@
+package dev.cadastros.SistemaDeCadastro;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Bora estudar!";
+    }
+}
